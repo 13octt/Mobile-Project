23771471a7f9c724559a6e13d8cd5f2e254883fb
Tzuy284 <93607267+Tzuy284@users.noreply.github.com>
2022-12-03 15:16:28 +0700

    load giao dien

---
+++ app/src/main/java/com/example/sampleproject/LoadingActivity.java
@@ -0,0 +1,24 @@
+package com.example.sampleproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.begin_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(LoadingActivity.this, LoginActivity.class));
+                finish();
+            }
+        },10000);
+    }
+}
